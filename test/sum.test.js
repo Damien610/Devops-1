55332c709b49029d8b5cc9bd742d39c571e9623c
Damien le padawan du coding <nerridamien@gmail.com>
2025-06-20 15:56:02 +0200

    rs

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,10 +1,10 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
+
 const sum = require('../sum');
 
-console.log('Résultat :', sum(2, 3)); // Optionnel, juste pour tester manuellement
 
-if (sum(2, 3) !== 5) {
-  throw new Error('Test échoué : sum(2, 3) doit être égal à 5');
-} else {
-  console.log('✅ Test passé !');
-}
+describe('sum()', () => {
+  test('adds 2 + 3 to equal 5', () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+});
