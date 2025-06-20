70fc22b73875953607e96e8f9eccc46815eee619
Damien le padawan du coding <nerridamien@gmail.com>
2025-06-20 16:33:57 +0200

    build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,20 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } }
+  {
+    ignores: ['dist/**'],
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser
+    },
+    plugins: {
+      js
+    },
+    rules: {
+      ...js.configs.recommended.rules
+    }
+  }
 ]);
